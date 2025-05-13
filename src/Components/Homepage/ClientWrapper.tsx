7decf9133f350f6645cb2ad49759f235ca56ea2f
Ashu-tosh1 <ashutos1201@gmail.com>
2025-05-13 20:37:11 +0530

    deploy

--- src/Components/Homepage/ClientWrapper.tsx
+++ src/Components/Homepage/ClientWrapper.tsx
@@ -1,8 +1,7 @@
 "use client"
 
 import React from "react";
-import { useSmoothScroll } from "./smoothscrool";
-
+import { useSmoothScroll } from "./SmoothScrool";
 
 const ClientWrapper = ({children}:{children:React.ReactNode}) => {
 useSmoothScroll()
