276c845908a50523ad7ba1c1fb9247c5eb58ba49
Ashu-tosh1 <ashutos1201@gmail.com>
2025-05-13 22:57:22 +0530

    check this

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
+import CardInfoSection from '@/Components/Homepage/CardInfoSection'
+import ContentSection from '@/Components/Homepage/ContentSection'
+import Footer from '@/Components/Homepage/Footer'
+import HeroSection from '@/Components/Homepage/HeroSection'
+import Navbar from '@/Components/Homepage/Navbar'
+import SliderSection from '@/Components/Homepage/SliderSection'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+      <div>
+      <Navbar/>
+      <HeroSection/>
+      <ContentSection/>
+      <CardInfoSection/>
+      <SliderSection/>
+      <Footer/>
+          
+    </div>
   )
 }
 
