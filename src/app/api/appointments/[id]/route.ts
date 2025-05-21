4ea360a49ed3acb4c1079813406f02475dce28b9
Ashu-tosh1 <ashutos1201@gmail.com>
2025-05-21 15:23:01 +0530

    now

--- src/app/api/appointments/[id]/route.ts
+++ src/app/api/appointments/[id]/route.ts
@@ -1,14 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
-  const appointmentId = params.id;
+  request: NextRequest,
+  context: { params: { id: string } }
+) {const appointmentId = context.params.id;
 
   if (!appointmentId) {
     return NextResponse.json(
